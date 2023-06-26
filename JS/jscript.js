edf8a3d402a745bbf2fb5053c816c28fa4c032cf
amir mohammad <amirmohammad@gamil.com>
2023-06-26 07:31:07 +0330

    end project

---
+++ JS/jscript.js
@@ -0,0 +1,22 @@
+//JavaScript
+
+$("#skills-flip").click(function(){
+	$("#skills-panel").slideToggle("slow");
+	$("#expand-sk").fadeToggle();
+});
+
+$("#exp-flip").click(function(){
+	$("#exp-panel").slideToggle("slow");
+	$("#expand-ex").fadeToggle();
+});
+
+$("#cert-flip").click(function(){
+	$("#cert-panel").slideToggle("slow");
+	$("#expand-ce").fadeToggle();
+});
+
+$("#edu-flip").click(function(){
+	$("#edu-panel").slideToggle("slow");
+	$("#expand-edu").fadeToggle();
+});
+
